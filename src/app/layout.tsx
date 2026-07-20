2021b787fdcc2a42f5a263ad9d831f2d577f6764
deniedae <deniedae@gmail.com>
2026-07-21 02:26:20 +1000

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Sidebar from "@/components/sidebar";
+import { Analytics } from "@vercel/analytics/next"
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       {children}
     </main>
   </div>
+  <Analytics />
 </body>
     </html>
   );
