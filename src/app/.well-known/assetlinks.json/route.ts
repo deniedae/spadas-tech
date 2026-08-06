bc6d44dec0eec8402c2d1d266c41622484baaeb5
deniedae <deniedae@gmail.com>
2026-08-06 19:28:49 +1000

    fix: syntax typo in /.well-known/assetlinks.json/route.ts

--- src/app/.well-known/assetlinks.json/route.ts
+++ src/app/.well-known/assetlinks.json/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
       target: {
         namespace: "android_app",
         package_name: "com.spadas.ai",
-        sha256_cert_fingerprints": [
+        sha256_cert_fingerprints: [
           "62:F6:E5:C9:2C:4A:F3:C0:E0:00:06:D0:4D:F7:72:65:57:88:47:12:36:4F:56:E4:32:8C:30:C9:3D:33:09:D6"
         ]
       }
